cd1952697d195a000bcf037477d06e57e6256364
Kieran Davies <vestiahd@gmail.com>
2017-06-20 14:51:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var request = require('request');
+
+//slack bot credentials
+var url = "https://hooks.slack.com/services/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"
+
+
+var app = express();
+
+const PORT = 4390;
+
+//start up server
+
+app.listen(PORT, function () {
+  console.log("Slack bot is up and running");
+});
+app.GET('/', function (req, res) {
+  //this sends the json response
+  request({
+    url: url,
+    method: "POST",
+    json: true,
+    headers: {
+      "content-type": "application/json",
+    },
+    body:{
+      text: "data"
+    }
+  })
+  //stil need this in for now. will try fixing
+  res.redirect('/');
+})
